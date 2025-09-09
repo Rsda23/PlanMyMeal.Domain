9c2eab96a8398b2e01359835926d8b6b5230cc28
Rsda23 <rsda23.0@gmail.com>
2025-09-09 10:44:20 +0200

    feat: add ingredient model

---
+++ Models/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PlanMyMeal.Domain.Models
+{
+    public class Ingredient
+    {
+        [JsonPropertyName("recipeId")]
+        public string RecipeId { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        public Ingredient() 
+        {
+        
+        }
+
+        public Ingredient(string title)
+        {
+            Title = title;
+        }
+    }
+}
