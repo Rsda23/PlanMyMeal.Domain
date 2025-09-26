d01972a6fccd636052796231354111f5eee1504f
Rsda23 <rsda23.0@gmail.com>
2025-09-26 10:55:02 +0200

    feat(recipe): add properties to model and overload constructor for full recipe creation

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -8,10 +8,40 @@ namespace PlanMyMeal.Domain.Models
         public string RecipeId { get; set; }
 
         [JsonPropertyName("userId")]
-        public string UserId { get; set; }
+        public string UserId { get; set; } = string.Empty;
 
         [JsonPropertyName("title")]
         public string Title { get; set; }
+
+        [JsonPropertyName("imageUrl")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [JsonPropertyName("createdAt")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [JsonPropertyName("updateAt")]
+        public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+
+        [JsonPropertyName("difficulty")]
+        public string Difficulty { get; set; }
+
+        [JsonPropertyName("isFavorite")]
+        public bool IsFavorite { get; set; }
+
+        [JsonPropertyName("totalTimeMinutes")]
+        public int TotalTimeMinutes { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("calories")]
+        public int Calories { get; set; }
+
+        [JsonPropertyName("diets")]
+        public List<string> Diets { get; set; } = new();
+
+        [JsonPropertyName("tags")]
+        public List<string> Tags { get; set; } = new();
 
         [JsonPropertyName("ingredients")]
         public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
@@ -22,10 +52,24 @@ namespace PlanMyMeal.Domain.Models
 
         }
 
-        public Recipe(string userId, string title, List<Ingredient> ingredients)
+        public Recipe(string title, string difficulty,string type)
+        {
+            Title = title;
+            Difficulty = difficulty;
+            Type = type;
+        }
+
+        public Recipe(string userId, string title, string imageUrl, string difficulty, int totalTimeMinutes, string type, int calories, List<string> diets, List<string> tags, List<Ingredient> ingredients)
         {
             UserId = userId;
             Title = title;
+            ImageUrl = imageUrl;
+            Difficulty = difficulty;
+            TotalTimeMinutes = totalTimeMinutes;
+            Type = type;
+            Calories = calories;
+            Diets = diets;
+            Tags = tags;
             Ingredients = ingredients;
         }
     }
